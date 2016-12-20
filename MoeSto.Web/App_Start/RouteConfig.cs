e6e27c531d1ec92d21ec2931b1d63851bdb701e9
Yulia Kapura <kapura.yulya@gmail.com>
2016-12-20 05:23:35 -0800

    add macket

--- MoeSto.Web/App_Start/RouteConfig.cs
+++ MoeSto.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MoeSto.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MoeSto", action = "Main", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MoeSto.Web/Controllers/MoeStoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoeSto.Web.Controllers
+{
+    public class MoeStoController : Controller
+    {
+        //
+        // GET: /MoeSto/
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+    }
+}
