91c44a28ea5f875823c65d8c53295d1f876b7269
Yulia Kapura <kapura.yulya@gmail.com>
2017-01-04 01:12:36 -0800

    add dto objects

--- MoeSto.Web/Models/DtoObjects/YPointCollection.cs
+++ MoeSto.Web/Models/DtoObjects/YPointCollection.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MoeSto.Web.Models
+namespace MoeSto.Web.Models.DtoObjects
 {
     public class YPointCollection
     {
         public string Type { get; set; }
-        public List<YPoint> Features { get; set; }
+        public YPoint[] Features { get; set; }
     }
 }
---
+++ MoeSto.Web/Models/DtoObjects/YPointProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoeSto.Web.Models.DtoObjects
+{
+    public class YPointProperties
+    {
+        public string BalloonContent { get; set; } = "Balloon content";
+        public string ClusterCaption { get; set; } = "Placemark 1";
+        public string HintContent { get; set; } = "Hint text";
+    }
+}
