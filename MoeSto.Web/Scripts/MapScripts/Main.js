3fafb1789d94a9667598ef01857143245c3ec460
Yulia Kapura <kapura.yulya@gmail.com>
2017-01-09 08:17:50 -0800

    cluster behavior

--- MoeSto.Web/Scripts/MapScripts/Main.js
+++ MoeSto.Web/Scripts/MapScripts/Main.js
@@ -4,9 +4,6 @@
 ymaps.ready(init);
 
 function init() {
-
-   
-
     function getBalloonContentBody(data) {
         return '<div class="companyDetailsWrapper"><div class="companyDetailsImg imgWrapper"><p class="companyDetailsName">' +
             (data.Name != null ? data.Name : "") +
@@ -17,9 +14,21 @@ function init() {
             (data.Email != null ? data.Email : "") + '</a></div></div>';
 
     }
-    function getBalloonContentFooter(data) {
-        return '<p>'+  '</p>' ;
+
+    function isClusterized(features) {
+        for (var i = 0; i < features.length; i++) {
+            var latitude = features[i].geometry.coordinates[0];
+            var longitude = features[i].geometry.coordinates[1];
+            for (var j = 1; j < features.length; j++) {
+                if (latitude != features[j].geometry.coordinates[0] && longitude != features[j].geometry.coordinates[1]) {
+                    return true;
+                }
+            }
+             return false;
+        }
+        return false;
     }
+
     ymaps.geolocation.get().then(function (res) {
         var mapContainer = $('#map'),
 
@@ -31,18 +40,27 @@ function init() {
                 [mapContainer.width(), mapContainer.height()]
             );
         createMap(mapState);
+        //Layout for carousel
+        var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+        // The "raw" flag means that data is inserted "as is" without escaping HTML.
+            //'<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+            '<div class=ballon_body><div class=ballon_header>{{properties.balloonContentHeader}}</div>{{ properties.balloonContentBody|raw }}</div>' //+
+            //'<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+        );
         var loadingObjectManager = new ymaps.LoadingObjectManager('Map/tile?bbox=%b', {
+            splitRequests: true,
             // Enabling clusterization.
             clusterize: true,
-            splitRequests: true,
-            // Cluster options are set with the 'cluster' prefix.
-            clusterHasBalloon: false,
-            // Object options are set with the geoObject prefix.
+            //options for GeoObjects
             geoObjectOpenBalloonOnClick: false,
+            geoObjectHideIconOnBalloonOpen: false,
+            //options foe clusters
+            clusterHasBalloon: true,
+            clusterDisableClickZoom: false,
             clusterOpenBalloonOnClick: false,
-            
-            
-           
+            clusterHideIconOnBalloonOpen: false,
+            clusterBalloonContentLayout: 'cluster#balloonCarousel',
+            clusterBalloonItemContentLayout: customItemContentLayout,
         });
         loadingObjectManager.objects.events.add(['click'], function(e){
             var objectId = e.get('objectId'),
@@ -52,13 +70,11 @@ function init() {
                 mapController.getCompanyDetailsById(
                     objectId,
                     function(data) {
-                        
                         object.properties = {
                             //balloonContentHeader: getBalloonContentHeader(data),
                             balloonContentBody: getBalloonContentBody(data),
                             ballonContentFooter: getBalloonContentFooter(data)
                         };
-                        
                         loadingObjectManager.objects.balloon.open(objectId);
                        
                     },
@@ -74,34 +90,42 @@ function init() {
                 cluster = loadingObjectManager.clusters.getById(clusterId),
                 //get cluster coordinates (?)
                 clusterCoords = cluster.features[0].geometry.coordinates;
-            
+            if (!isClusterized(cluster.features)) {
+                
                 var mapController = new MapController("map/GetCompanyDetailsByCoordinates");
                 mapController.getCompanyDetailsByCoordinates(
                     clusterCoords,
-                    function (data) {
-                        
+                    function(data) {
+
                         var clusterId = e.get('objectId');
                         var cluster = loadingObjectManager.clusters.getById(clusterId);
-                        var clusterFeatures = cluster.features;
-                        for (var j = 0; j < clusterFeatures.length; j++) {
-                            clusterFeatures[0].properties = {
-                                //balloonContentHeader: getBalloonContentHeader(data),
-                                balloonContentBody: getBalloonContentBody(data[j]) 
-                                
-                            };
+
+                        if (cluster && cluster.features) {
+                            var clusterFeatures = cluster.features;
+                            for (var i = 0; i < clusterFeatures.length; i++) {
+                                clusterFeatures[i].properties = {
+                                    balloonContentHeader: data[i].Name,
+                                    balloonContentBody: '<p>' + data[i].Address + ' ' + data[i].Phones + '</p>'
+                                }
+                            }
+                            if (myMap.getZoom() == 19) {
+                                loadingObjectManager.clusters.balloon.open(clusterId);
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            return false;
                         }
-                        //loadingObjectManager.clusters.balloon.open(cluster.id);
-                        loadingObjectManager.objects.balloon.open(clusterFeatures[0].id);
+                            
+                           
                     },
                     function() {}
                 );
+                
+            }
             
         });
-         
-
-        function hasBalloonData(objectId) {
-            return loadingObjectManager.objects.getById(objectId).properties.balloonContent;
-        }
+       
         myMap.geoObjects.add(loadingObjectManager);
         
     }, function (e) { 
