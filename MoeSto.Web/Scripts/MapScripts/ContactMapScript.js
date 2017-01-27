39d3d5e625cc32b97fc4ff86a2c86b1b063f5105
Yulia Kapura <kapura.yulya@gmail.com>
2017-01-27 06:47:12 -0800

    find coordinates draggable geoobject

--- MoeSto.Web/Scripts/MapScripts/ContactMapScript.js
+++ MoeSto.Web/Scripts/MapScripts/ContactMapScript.js
@@ -1,19 +1,57 @@
 ﻿ymaps.ready(init);
-var myMap, myPlacemark;
+var myMap, myGeoObject;
 
 function init() {
-    var details = $('#comapnyDetailsHdn').val();
-    myMap = new ymaps.Map("contact-map", {
-        center: [details],
-        zoom:12
-    });
     
-    myPlacemark = new ymaps.Placemark([details], {
-        hintContent: 'Москва!',
-        balloonContent: 'Столица России'
+    var oldLatitude = $('#hdnLatitude').text();
+    var oldLongitude = $('#hdnLongitude').text();
+    $('#latitude').val(oldLatitude);
+    $('#longitude').val(oldLongitude);
+    myMap = new ymaps.Map("contact-map", {
+        center: [oldLatitude, oldLongitude],
+        zoom: 12,
+        controls: ['zoomControl', 'typeSelector', 'fullscreenControl', "searchControl"]
+    }, {
+        searchControlProvider: 'yandex#search'
     });
+    var searchControl = new ymaps.control.SearchControl({
+        options: {
+            // The search will be performed across toponyms and businesses.
+            provider: 'yandex#search'
+        }
+    });
+    myGeoObject = new ymaps.GeoObject({
+        // Описание геометрии.
+        geometry: {
+            type: "Point",
+            coordinates: [oldLatitude, oldLongitude]
+        },
+        // Свойства.
+        properties: {
+            // Контент метки.
 
-    myMap.geoObjects.add(myPlacemark);
+        }
+    }, {
+        // Опции.
+        // Иконка метки будет растягиваться под размер ее содержимого.
+        preset: 'islands#dotIcon',
+        iconColor: '#735184',
+        // Метку можно перемещать.
+        draggable: true
+    });
+    myGeoObject.events.add("dragstart", function (event) {
+        var tempLatitude = $('#latitude').val(),
+            tempLongitude = $('#longitude').val();
+        $('#latitude').val("");
+        $('#longitude').val("");
+    });
+    myGeoObject.events.add("dragend", function (event) {
+        var newLatitude = myGeoObject.geometry._coordinates[0],
+            newLongitude = myGeoObject.geometry._coordinates[1];
+        var myReverseGeocoder = ymaps.geocode([newLatitude, newLongitude]);
+        $('#latitude').val(newLatitude);
+        $('#longitude').val(newLongitude);
+    });
+    myMap.geoObjects.add(myGeoObject);
+
 }
-
-
