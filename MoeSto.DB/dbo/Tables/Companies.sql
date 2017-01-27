11177b961d9e6a5733f35709e99f974a673ee66d
Yulia Kapura <kapura.yulya@gmail.com>
2017-01-27 04:02:45 -0800

    Revert "Revert "gallery""
    This reverts commit 78fa0d7c01c53b1a9827a2306e9ee55e308d165d.

--- MoeSto.DB/dbo/Tables/Companies.sql
+++ MoeSto.DB/dbo/Tables/Companies.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Companies] (
-    [Id]        INT        NOT NULL,
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
     [Latitude]  FLOAT (53) NOT NULL,
     [Longitude] FLOAT (53) NOT NULL,
     CONSTRAINT [PK_Companies] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
--- MoeSto.Web/Controllers/CompanyController.cs
+++ MoeSto.Web/Controllers/CompanyController.cs
@@ -1,6 +1,12 @@
-﻿using System.Linq;
+﻿using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using MoeSto.DAC;
+using MoeSto.Web.Controllers.UtilsMethods;
+using MoeSto.Web.Models;
 
 namespace MoeSto.Web.Controllers
 {
@@ -21,6 +27,24 @@ namespace MoeSto.Web.Controllers
             ViewBag.CompanyDetails = dbContext.CompanyDetails.FirstOrDefault(x => x.Id.Equals(id));
             return View();
         }
+        [HttpPost]
+        public ActionResult UploadImgs()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
 
+                if (file != null && file.ContentLength > 0)
+                {
+                    dbContext.Images.InsertOnSubmit(Utils.GetImgFromRequst(Request, file));
+                    dbContext.SubmitChanges();
+                }
+            }
+            //in json result pass needed information for description of image 
+            return Json(new { text = "boom!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        
     }
+
 }
